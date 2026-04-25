4ee66b84b9caf89a8655a7d6abf59ef5f65dce1c
RndBE <rnd.software.be@gmail.com>
2026-04-25 14:51:05 +0700

    feat: implement irrigation node data management and diagnostic scripts for PetaController integration

---
+++ test2.php
@@ -0,0 +1,1 @@
+<?php require 'vendor/autoload.php'; $app = require_once 'bootstrap/app.php'; $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class); $kernel->bootstrap(); $pc = app(App\Http\Controllers\PetaController::class); $res = $pc->index(); print_r($res->getData()['points']->where('id_logger', '10367')->first());
